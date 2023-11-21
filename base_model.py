310bd857e2982b4c75bc508be6afae0ea5aa918d
andreignatius <andre.callme@gmail.com>
2023-11-21 21:24:52 +0800

    tf oop first draft

--- base_model.py
+++ base_model.py
@@ -12,6 +12,15 @@ from sklearn.metrics import roc_curve, auc
 from sklearn.model_selection import cross_val_score
 from imblearn.over_sampling import SMOTE
 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import LabelEncoder
+
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+
+
 class BaseModel:
     def __init__(self, file_path, model_type):
         self.file_path = file_path
@@ -33,7 +42,10 @@ class BaseModel:
             self.model = LogisticRegression(class_weight='balanced')
         elif self.model_type == 'gbt':
             self.model = GradientBoostingClassifier()
-
+        elif self.model_type == 'tf_nn':
+            self.model = Sequential()
+            self.label_encoder = LabelEncoder()
+            
 
     def load_preprocess_data(self):
         # Implement data loading logic
@@ -112,15 +124,45 @@ class BaseModel:
         # Data Preprocessing
         final_dataset_with_new_features.dropna(inplace=True)
 
+        checkpoint_date_bottom = None  # Initialize to a sensible default or first date
+        checkpoint_date_top = None  # Initialize to a sensible default or first date
+
+        for index, row in final_dataset_with_new_features.iterrows():
+            print("index: ", index, "row: ", row)
+            current_price = row['Open']
+
+            today_date = pd.to_datetime(row['Date'])
+
+            if row['Label'] == 'Buy':
+                checkpoint_date_bottom = today_date
+                print("CHECK UPDATE local_trough checkpoint_date_bottom: ", today_date)
+            if row['Label'] == 'Sell':
+                checkpoint_date_top = today_date
+                print("CHECK UPDATE local_peak checkpoint_date_top: ", today_date)
+
+            days_since_bottom = (today_date - checkpoint_date_bottom).days if checkpoint_date_bottom else 0
+            print("days_since_bottom: ", days_since_bottom)
+            days_since_peak = (today_date - checkpoint_date_top).days if checkpoint_date_top else 0
+            print("days_since_peak: ", days_since_peak)
+
+            # final_dataset_with_new_features.at[index, 'DaysSincePeakTrough'] = max(days_since_bottom, days_since_peak)
+            final_dataset_with_new_features.at[index, 'DaysSincePeak'] = days_since_peak
+            final_dataset_with_new_features.at[index, 'DaysSincePeakTrough'] = days_since_bottom
+
         self.data = final_dataset_with_new_features
+
+        return self.data
 
     def train(self):
         # Implement or leave empty to override in derived classes
-        self.X = self.data[['Short_Moving_Avg', 'Long_Moving_Avg', 'RSI']]
+        # self.X = self.data[['Short_Moving_Avg', 'Long_Moving_Avg', 'RSI']]
+        self.X = self.data[['Short_Moving_Avg', 'Long_Moving_Avg', 'RSI', 'DaysSincePeak', 'DaysSincePeakTrough']]
+
         self.y = self.data['Label']
 
         # Splitting the dataset and standardizing features
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=0.3, random_state=42)
+        
         print("y_train value counts: ", self.y_train.value_counts())
         self.X_train_scaled = self.scaler.fit_transform(self.X_train)
         self.X_test_scaled = self.scaler.transform(self.X_test)
@@ -128,12 +170,49 @@ class BaseModel:
         if self.model_type == 'gbt':
             # Apply SMOTE
             smote = SMOTE()
-            self.X_train_scaled, self.y_train = smote.fit_resample(self.X_train_scaled, self.y_train)        
-        self.model.fit(self.X_train_scaled, self.y_train)
+            self.X_train_scaled, self.y_train = smote.fit_resample(self.X_train_scaled, self.y_train)
+
+        if self.model_type == 'logreg' or self.model_type == 'gbt':
+            self.model.fit(self.X_train_scaled, self.y_train)
+
+        if self.model_type == 'tf_nn':
+            # Convert labels to tensors and apply one-hot encoding
+            y_train_encoded = self.label_encoder.fit_transform(self.y_train)
+            y_test_encoded = self.label_encoder.transform(self.y_test)
+
+            # Convert labels to categorical (one-hot encoding)
+            y_train_categorical = to_categorical(y_train_encoded)
+            y_test_categorical = to_categorical(y_test_encoded)
+
+            # Neural Network architecture
+            self.model.add(Dense(64, input_dim=self.X_train_scaled.shape[1], activation='relu'))
+            self.model.add(Dense(32, activation='relu'))
+            self.model.add(Dense(y_train_categorical.shape[1], activation='softmax'))  # Output layer
+
+            # Compile the model
+            self.model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+            # Train the model
+            self.model.fit(self.X_train_scaled, y_train_categorical, epochs=50, batch_size=10)
+            
 
     def predict(self):
-        # Implement or leave empty to override in derived classes
-        self.data['PredictedLabel'] = self.model.predict(self.scaler.transform(self.X))
+        if self.model_type == 'logreg' or self.model_type == 'gbt':
+            # Implement or leave empty to override in derived classes
+            self.data['PredictedLabel'] = self.model.predict(self.scaler.transform(self.X))
+
+        if self.model_type == 'tf_nn':
+            print("CHECK TF_NN")
+            predicted_probs = self.model.predict(self.X_test_scaled)
+            # predicted_probs = self.model.predict(self.X)
+            # Convert probabilities to class labels
+            predicted_labels = np.argmax(predicted_probs, axis=1)
+
+            # Assuming label_encoder was used to encode y_train
+            predicted_categories = self.label_encoder.inverse_transform(predicted_labels)
+            print("CHECK predicted_labels: ", predicted_categories)
+            return predicted_categories
+
         return self.data
 
     def evaluate(self, X, y):
---
+++ tf_oop.py
@@ -0,0 +1,65 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import LabelEncoder
+
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+
+import pandas as pd
+import numpy as np
+from scipy.fft import fft
+from scipy.signal import find_peaks
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix
+
+# from imblearn.over_sampling import SMOTE
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.utils.class_weight import compute_class_weight
+
+import random
+
+from base_model import BaseModel
+
+# Load the Forex data
+file_path = 'USD_JPY.csv'
+model = BaseModel(file_path, 'tf_nn')
+
+data = model.load_preprocess_data()
+model.train()
+print("GOING TO PREDICT")
+predicted_categories = model.predict()
+
+# Backtesting with stop-loss and take-profit
+stop_loss_threshold = 0.05  # 5% drop from buying price
+take_profit_threshold = 0.05  # 5% rise from buying price
+cash = 10000  # Starting cash
+trading_lot = 2000
+shares = 0    # Number of shares held
+trade_log = []  # Log of trades
+
+for index, (row, prediction) in enumerate(zip(data.iterrows(), predicted_categories)):
+    # print("row: ", row)
+    # print("row[0]: ", row[0])
+    # print("row[1]: ", row[1])
+    current_price = row[1]['Open']
+    
+    if prediction == 'Buy' and cash >= trading_lot:
+        num_shares_to_buy = int(trading_lot / current_price)
+        shares += num_shares_to_buy
+        cash -= num_shares_to_buy * current_price
+        buy_price = current_price
+        trade_log.append(f"Buy {num_shares_to_buy} shares at {current_price} on {row[1]['Date']}")
+    elif prediction == 'Sell' and shares > 0:
+        cash += shares * current_price
+        trade_log.append(f"Sell {shares} shares at {current_price} on {row[1]['Date']}")
+        shares = 0
+
+# Calculate final portfolio value
+final_portfolio_value = cash + (shares * data.iloc[-1]['Open'])
+
+# Output
+print(trade_log)
+print(f"Final Portfolio Value: {final_portfolio_value}")

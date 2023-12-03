f8abb48b2c8a8bd485b9edb3a888da8edeb6b199
andreignatius <andre.callme@gmail.com>
2023-12-03 22:44:25 +0800

    add stop loss and margin call checks

--- backtest/trading_strategy.py
+++ backtest/trading_strategy.py
@@ -1,10 +1,11 @@
 
 class TradingStrategy:
 
-    def __init__(self, model, data, start_cash=10000, trading_lot=2500, stop_loss_threshold=0.05, take_profit_threshold=0.05, leverage_factor=3):
+    def __init__(self, model, data, start_cash=10000, trading_lot=2500, stop_loss_threshold=0.05, take_profit_threshold=0.05, leverage_factor=3, margin_call_threshold=0.5):
         self.model = model
         self.data = data
         self.cash = start_cash
+        self.margin_requirement = start_cash * margin_call_threshold
         self.starting_cash = start_cash
         self.trading_lot = trading_lot * leverage_factor  # Adjusted for leverage
         self.stop_loss_threshold = stop_loss_threshold
@@ -19,19 +20,21 @@ class TradingStrategy:
         print("CASH000: ", self.cash)
         for index, (row, prediction) in enumerate(zip(self.data.iterrows(), predicted_categories)):
             usd_jpy_spot_rate = row[1]['Open']
+            current_date = row[1]['Date']
 
-            # if self.jpy_inventory > 0:
-            #     change_percentage = (usd_jpy_spot_rate - self.buy_price) / self.buy_price
-            #     if change_percentage <= -self.stop_loss_threshold:
-            #         self.cash += self.jpy_inventory * usd_jpy_spot_rate
-            #         self.trade_log.append(f"Sell {self.jpy_inventory} shares at {usd_jpy_spot_rate} on {row[1]['Date']} (Stop-loss triggered)")
-            #         self.jpy_inventory = 0
-            #         continue
+            is_stop_loss_triggered = self._check_stop_loss(usd_jpy_spot_rate, current_date)
+
+            if is_stop_loss_triggered:
+                continue
 
             if prediction == 'Sell' and self.cash >= self.trading_lot and ( self.buy_price is None or (self.buy_price is not None and ( usd_jpy_spot_rate < self.buy_price * 0.99 or usd_jpy_spot_rate > self.buy_price * 1.01) ) ):
-                self._buy_jpy(usd_jpy_spot_rate, row[1]['Date'])
+                self._buy_jpy(usd_jpy_spot_rate, current_date)
             elif prediction == 'Buy' and self.jpy_inventory > 0:
-                self._sell_jpy(usd_jpy_spot_rate, row[1]['Date'])
+                self._sell_jpy(usd_jpy_spot_rate, current_date)
+
+            if self._check_margin_call(usd_jpy_spot_rate):
+                print("MARGIN CALL!!! this should not happen!")
+                self._sell_jpy(usd_jpy_spot_rate, current_date)
 
     def _buy_jpy(self, rate, date):
         jpy_bought = int(self.trading_lot * rate)
@@ -50,6 +53,23 @@ class TradingStrategy:
         self.trade_log.append(f"Sell {self.jpy_inventory} JPY at {rate} on {date} ({sell_reason})")
         self.jpy_inventory = 0
 
+    def _compute_mtm(self, usd_jpy_spot_rate):
+        return self.cash + ( self.jpy_inventory / usd_jpy_spot_rate )
+
+    def _check_stop_loss(self, usd_jpy_spot_rate, date):
+        if self.jpy_inventory > 0:
+            change_percentage = (usd_jpy_spot_rate - self.buy_price) / self.buy_price
+            if change_percentage <= -self.stop_loss_threshold:
+                self._sell_jpy(usd_jpy_spot_rate, date, forced=True)
+                return True
+        return False
+
+    def _check_margin_call(self, usd_jpy_spot_rate):
+        if self.jpy_inventory > 0:
+            if self._compute_mtm(usd_jpy_spot_rate) < self.margin_requirement:
+                return True
+        return False
+
     # def _apply_interest_charge(self):
     #     days_held = len(self.daily_return_factors)
     #     daily_interest_rate = (1 + self.annual_interest_rate) ** (1/365) - 1
@@ -58,7 +78,8 @@ class TradingStrategy:
 
     def evaluate_performance(self):
         final_usd_jpy_spot_rate = self.data.iloc[-1]['Open']
-        final_portfolio_value = self.cash + (self.jpy_inventory / final_usd_jpy_spot_rate)
+        # final_portfolio_value = self.cash + (self.jpy_inventory / final_usd_jpy_spot_rate)
+        final_portfolio_value = self._compute_mtm(final_usd_jpy_spot_rate)
         print("cash: ", self.cash)
         print("shares: ", self.jpy_inventory)
         pnl_per_trade = (final_portfolio_value - self.starting_cash) / len(self.trade_log) if self.trade_log else 0

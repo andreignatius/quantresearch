1a8b7fc38d04c3335e70c654de92fa71ce2fd2f1
andreignatius <andre.callme@gmail.com>
2023-12-03 17:35:51 +0800

    wip fix for forex trading label

--- usd_jpy_yahoo_finance.py
+++ usd_jpy_yahoo_finance.py
@@ -1,17 +1,20 @@
 import yfinance as yf
 import pandas as pd
 
-# Define the ticker symbol for USD to JPY exchange rate
-ticker_symbol = 'USDJPY=X'
+# # Define the ticker symbol for USD to JPY exchange rate
+# ticker_symbol = 'USDJPY=X'
+
+# Define the ticker symbol for JPY to USD exchange rate
+ticker_symbol = 'JPYUSD=X'
 
 # Fetch data from Yahoo Finance
-data = yf.download(ticker_symbol, start='1990-01-01', end='2023-11-27')
+data = yf.download(ticker_symbol, start='1990-01-01', end='2023-12-01')
 
 # Reset index to have Date as a column
 data.reset_index(inplace=True)
 
 # Save the data to a CSV file under 'backtest/data'
-file_path = 'backtest/data/USD_JPY_YF.csv'
+file_path = 'backtest/data/JPY_USD_YF.csv'
 data.to_csv(file_path, index=False)
 
 print(f"Exchange rate data saved to {file_path}")
